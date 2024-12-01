0d19aff8b26abd3ac0adc877377907fb11ea9bc4
elisey-moyseenko <167004811+elisey-moyseenko@users.noreply.github.com>
2024-12-01 19:47:31 +0300

    Add files via upload

---
+++ git.py
@@ -0,0 +1,18 @@
+from random import randint
+rn_min = 1
+rn_max = 10
+turns = 10
+rn = randint(rn_min,rn_max)
+
+print('Я загадал число от 1 до 10')
+print('Угадай его за',turns, 'попыток')
+
+for i in range(turns):
+    try:
+        n = int(input(f'Какое число загадано({rn_min}...{rn_max}?)\n'))
+        if n == rn:
+            print('Точно,вы угадали с',(i + 1), 'попытки')
+            break
+    except:
+        print('вы ввели не число')
+    print('У вас осталось',turns - (i + 1), 'попыток')
